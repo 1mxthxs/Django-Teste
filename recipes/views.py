1bdd8ec5b37d674087aaa7e3c1476ee25255529b
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-04-05 20:46:32 -0400

    tittle block

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,7 @@ def Home(request):
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Matheus Ricardo'
     })
 
