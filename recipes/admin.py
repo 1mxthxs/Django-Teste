2d0a49343cfbaf67a57b264192ae42c65e09da4c
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-04-06 11:03:20 -0400

    Entidades/Models em Admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, RecipeItem
 
 
 class CategoryAdmin(admin.ModelAdmin):
     admin.site.register(Category)
+
+
+class RecipeItemAdmin(admin.ModelAdmin):
+    admin.site.register(RecipeItem)
--- recipes/models.py
+++ recipes/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -16,7 +17,6 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -29,3 +29,30 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
         )
+
+
+class RecipeItem(models.Model):
+    
+
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65, unique=True, null=True)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    uptaded_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+        )
+    
+    def __str__(self):
+        return self.title
